5ddc5910375e6ec6df6dfcb28d7b21a151bb064d
Hitu-C <robby@robby.blue>
2023-11-07 20:18:22 -0600

    Update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 
 let db;
-const port = 3200;
-const uri = "mongodb://Tab:@ac-9wodkrn-shard-00-00.a7lfbvs.mongodb.net:27017,ac-9wodkrn-shard-00-01.a7lfbvs.mongodb.net:27017,ac-9wodkrn-shard-00-02.a7lfbvs.mongodb.net:27017/?ssl=true&replicaSet=atlas-10su2o-shard-0&authSource=admin&retryWrites=true&w=majority";
+const port = process.env.PORT;
+const uri = "mongodb://Tab:" + process.env.DBPASS + "@ac-9wodkrn-shard-00-00.a7lfbvs.mongodb.net:27017,ac-9wodkrn-shard-00-01.a7lfbvs.mongodb.net:27017,ac-9wodkrn-shard-00-02.a7lfbvs.mongodb.net:27017/?ssl=true&replicaSet=atlas-10su2o-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 
 const client = new MongoClient(uri, {
@@ -16,16 +17,24 @@ const client = new MongoClient(uri, {
 });
 
 app.get("/api/users", async (req, res) => {
-  const users = client.db("tabtracker").collection('users').find({});
-  res.json(users);
+  try {
+    console.log("Getting users...");
+    const usersCursor = db.collection('users').find({});
+    const users = await usersCursor.toArray();
+    // {"error":"Cannot read properties of undefined (reading 'collection')"}
+    console.log("Users obtained")
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 async function start() {
 
   try {
     await client.connect();
-    db = client.db()
-    await client.db("tabtracker").command({ ping: 1 });
+    const db = client.db("tabtracker")
+    await db.command({ ping: 1 });
     console.log("Connected to MongoDB!");
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
